7e9a4e7aeb57ad26eb2d93f531695da097b4500f
Jess Heron <jess.heron@circle.com>
2023-06-01 09:15:51 -0700

    updates url for edit this page linksl.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,14 +49,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/jheron-circle/docs-spike',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/jheron-circle/docs-spike',
         },
         theme: {
           customCss: require.resolve('./src/css/custom-circle.css'),
